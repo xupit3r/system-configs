2aa2a36b558583660faf8bf77d96c59e57208cb4
Joe D'Alessandro <joe@thejoeshow.net>
2023-04-23 10:32:40 -0400

    added in weather icons!  * using "Weather Icons" font: aur/ttf-weather-icons  * altered weather display/format

--- eww/bar/scripts/weather.clj
+++ eww/bar/scripts/weather.clj
@@ -9,41 +9,52 @@
 (def API_BASE "https://api.open-meteo.com/v1/forecast?latitude=40.43&longitude=-79.93&hourly=temperature_2m,precipitation&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1")
 
 (def WMO {
-  0 	"Clear sky"
-  1   "Mainly clear, partly cloudy, and overcast"
-  2   "Mainly clear, partly cloudy, and overcast"
-  3 	"Mainly clear, partly cloudy, and overcast"
-  45  "Fog and depositing rime fog"
-  48 	"Fog and depositing rime fog"
-  51  "Drizzle: Light, moderate, and dense intensity"
-  53  "Drizzle: Light, moderate, and dense intensity"
-  55 	"Drizzle: Light, moderate, and dense intensity"
-  56  "Freezing Drizzle: Light and dense intensity"
-  57 	"Freezing Drizzle: Light and dense intensity"
-  61 	"Rain: Slight, moderate and heavy intensity"
-  63 	"Rain: Slight, moderate and heavy intensity"
-  65 	"Rain: Slight, moderate and heavy intensity"
-  66 	"Freezing Rain: Light and heavy intensity"
-  67 	"Freezing Rain: Light and heavy intensity"
-  71  "Snow fall: Slight, moderate, and heavy intensity"
-  73  "Snow fall: Slight, moderate, and heavy intensity"
-  75 	"Snow fall: Slight, moderate, and heavy intensity"
-  77 	"Snow grains"
-  80 	"Rain showers: Slight, moderate, and violent"
-  81 	"Rain showers: Slight, moderate, and violent"
-  82 	"Rain showers: Slight, moderate, and violent"
-  85 	"Snow showers slight and heavy"
-  86 	"Snow showers slight and heavy"
-  95  "* Thunderstorm: Slight or moderate"
-  96  "* Thunderstorm with slight and heavy hail"
-  99  "* Thunderstorm with slight and heavy hail"
+  0   {:description "Clear sky" :icon ""}
+  1   {:description "Mainly clear" :icon ""}
+  2   {:description "Partly cloudy" :icon ""}
+  3   {:description "Overcast" :icon ""}
+  45  {:description "Fog" :icon ""}
+  48 	{:description "Fog" :icon ""}
+  51  {:description "Drizzle: light" :icon ""}
+  53  {:description "Drizzle: moderate" :icon ""}
+  55 	{:description "Drizzle: dense intensity" :icon ""}
+  56  {:description "Freezing drizzle: light" :icon ""}
+  57 	{:description "Freezing drizzle: dense intensity" :icon ""}
+  61 	{:description "Rain: slight" :icon ""}
+  63 	{:description "Rain: moderate" :icon ""}
+  65 	{:description "Rain: heavy intensity" :icon ""}
+  66 	{:description "Freezing rain: light" :icon ""}
+  67 	{:description "Freezing rain: heavy intensity" :icon ""}
+  71  {:description "Snow fall: slight" :icon ""}
+  73  {:description "Snow fall: moderate" :icon ""}
+  75 	{:description "Snow fall: heavy intensity" :icon ""}
+  77 	{:description "Snow grains" :icon ""}
+  80 	{:description "Rain showers: slight" :icon ""}
+  81 	{:description "Rain showers: moderate" :icon ""}
+  82 	{:description "Rain showers: violent" :icon ""}
+  85 	{:description "Snow showers: slight" :icon ""}
+  86 	{:description "Snow showers: heeavy" :icon ""}
+  95  {:description "* Thunderstorm" :icon ""}
+  96  {:description "* Thunderstorm with slight hail" :icon ""}
+  99  {:description "* Thunderstorm with heavy hail" :icon ""}
 })
 
+
+(defn pull-temperature [data]
+  (int (Math/floor (:temperature (:current_weather data)))))
+
+(defn pull-units [data]
+  (:temperature_2m (:hourly_units data)))
+
+(defn pull-conditions [data]
+  (get WMO 
+       (:weathercode (:current_weather data)) 
+       {:description "Conditions unavailable." :icon ""}))
+
 (defn relevant [data] 
-  {:temperature (int (Math/floor (:temperature (:current_weather data))))
-   :units (:temperature_2m (:hourly_units data))
-   :conditions (get WMO (:weathercode (:current_weather data)) "?")
-})
+  {:temperature (pull-temperature data)
+   :units (pull-units data)
+   :conditions (pull-conditions data)})
 
 (defn decode [s] (json/parse-string s true))
 (defn encode [s] (json/generate-string s))
