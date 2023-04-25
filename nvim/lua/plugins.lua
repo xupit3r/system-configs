667141724d4a24ba10d2c949cf04e81285a8fc8c
Joe D'Alessandro <joe@thejoeshow.net>
2023-04-25 06:59:08 -0400

    added in some basic neovim conf  * packer for plugin management  * tokyonight theme

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,9 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- tokyo night theme
+  use 'folke/tokyonight.nvim'
+end)
