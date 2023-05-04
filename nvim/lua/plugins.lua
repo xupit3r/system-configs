0d40c6a57f8f48aefb0b860c8b7462a48200474d
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-04 08:34:41 -0400

    added telescope to nvim  * not much config yet, but it is pretty sweet...

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -140,4 +140,12 @@ return require('packer').startup(function()
       require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     end
   }
+
+  -- telscope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    }
+  }
 end)
