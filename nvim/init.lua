013afe07d3d897d951e35348a29588068d0f11ff
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-04 08:20:27 -0400

    added in "hop" to neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -361,6 +361,23 @@ npairs.setup({
     check_ts = true,
 })
 
+--
+-- Search config
+--
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
 
 --
 -- Rust stuffs
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -131,4 +131,13 @@ return require('packer').startup(function()
       'neovim/nvim-lspconfig'
     }
   }
+
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
 end)
