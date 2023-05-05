7f74732dc322fbaadf505f8e044a57d29b17881b
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-05 05:56:02 -0400

    started collecting extra installs for eovim  * not everything...will have to retroactively go back and add things in  * just trying to not lose anything...

---
+++ additional/nvim.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# install python stuffs
+pip3 install neovim --upgrade
+
+# install nodejs stuffs
+sudo npm i -g neovim
+
+# install ruby stuffs
+gem install neovim
+
+# some pakages
+sudo pacman -Syu ripgrep fd
