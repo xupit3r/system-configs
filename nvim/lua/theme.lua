f5f8c4a6e889e34024353df8f3af25bfa5f973ab
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-11 06:39:45 -0400

    adjusted some of the theme customization  * adjusted some colors/integrations for catppuccin

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,44 +1,44 @@
 -- setup the tokyonight theme
 require("tokyonight").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  style = "storm",
-  terminal_colors = true,
-  styles = {
-    comments = { italic = true },
-    keywords = { italic = true },
-    sidebars = "dark",
-    floats = "dark",
-  },
-  sidebars = { "qf", "help", "packer" },
+	-- your configuration comes here
+	-- or leave it empty to use the default settings
+	style = "storm",
+	terminal_colors = true,
+	styles = {
+		comments = { italic = true },
+		keywords = { italic = true },
+		sidebars = "dark",
+		floats = "dark",
+	},
+	sidebars = { "qf", "help", "packer" },
 })
 
-
 -- setup/alter catpuccin theme
-require('catppuccin').setup {
-  flavour = 'mocha',
-  color_overrides = {
-    mocha = {
-      base = "#000000",
-      mantle = "#000000",
-      crust = "#000000",
-    },
-  },
-  highlight_overrides = {
-    mocha = function(C)
-      return {
-        TabLineSel = { bg = C.pink },
-        CmpBorder = { fg = C.surface2 },
-        Pmenu = { bg = C.none },
-        TelescopeBorder = { link = "FloatBorder" },
-      }
-    end,
-  },
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true
-  }
-}
+require("catppuccin").setup({
+	flavour = "mocha",
+	color_overrides = {
+		mocha = {
+			base = "#000000",
+			mantle = "#000000",
+			crust = "#000000",
+		},
+	},
+	styles = {
+		comments = { "italic" },
+		conditionals = { "italic" },
+		functions = { "italic" },
+		keywords = { "bold" },
+	},
+	custom_highlights = function(colors)
+		return {
+			TabLineSel = { bg = colors.peach },
+		}
+	end,
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		telescope = true,
+	},
+})
 
-vim.cmd [[ colorscheme catppuccin ]]
+vim.cmd([[ colorscheme catppuccin ]])
