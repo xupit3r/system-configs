e8417ce1a29d32d9f7883a3408c2edd4f9ba8660
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-13 14:56:10 -0400

    finally found the random text culprit! 🥳  * it was a "spell" source in null-ls  * it has since been removed

--- nvim/lua/trees.lua
+++ nvim/lua/trees.lua
@@ -1,17 +1,17 @@
 -- treesitter
-require('nvim-treesitter.configs').setup {
-  ensure_installed = "all",
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting=false,
-  },
-  ident = { 
-    enable = true 
-  }, 
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  }
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "all",
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	ident = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
+})
