b92a49527448d306edded2baea2d58e0c03a6dd4
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-19 15:14:47 -0400

    cleaned up some stuff for the weather script

--- eww/bar/scripts/weather.clj
+++ eww/bar/scripts/weather.clj
@@ -5,40 +5,50 @@
   '[cheshire.core :as json])
 
 ;; this will retrieve weather data from Open Meteo
+(def API_BASE "https://api.open-meteo.com/v1/forecast")
+(def API_QUERY 
+  (str
+    "?latitude=40.43"
+    "&longitude=-79.93"
+    "&hourly=temperature_2m,precipitation"
+    "&current_weather=true"
+    "&temperature_unit=fahrenheit"
+    "&windspeed_unit=mph"
+    "&precipitation_unit=inch"
+    "&forecast_days=1"))
+(def API_URL (str API_BASE API_QUERY))
+                 
 
-(def API_BASE "https://api.open-meteo.com/v1/forecast?latitude=40.43&longitude=-79.93&hourly=temperature_2m,precipitation&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1")
-
-(def WMO {
-  0   {:description "Clear sky" :icon "󰖙"}
-  1   {:description "Mainly clear" :icon "󰖐"}
-  2   {:description "Partly cloudy" :icon ""}
-  3   {:description "Overcast" :icon "󰖐"}
-  45  {:description "Fog" :icon "󰖑"}
-  48 	{:description "Fog" :icon "󰖑"}
-  51  {:description "Drizzle: light" :icon "󰖗"}
-  53  {:description "Drizzle: moderate" :icon "󰖗"}
-  55 	{:description "Drizzle: dense intensity" :icon "󰖗"}
-  56  {:description "Freezing drizzle: light" :icon "󰙿"}
-  57 	{:description "Freezing drizzle: dense intensity" :icon "󰙿"}
-  61 	{:description "Rain: slight" :icon "󰖖"}
-  63 	{:description "Rain: moderate" :icon "󰖖"}
-  65 	{:description "Rain: heavy intensity" :icon "󰖖"}
-  66 	{:description "Freezing rain: light" :icon "󰖒"}
-  67 	{:description "Freezing rain: heavy intensity" :icon "󰖒"}
-  71  {:description "Snow fall: slight" :icon "󰖘"}
-  73  {:description "Snow fall: moderate" :icon "󰖘"}
-  75 	{:description "Snow fall: heavy intensity" :icon "󰼶"}
-  77 	{:description "Snow grains" :icon "󰵼"}
-  80 	{:description "Rain showers: slight" :icon ""}
-  81 	{:description "Rain showers: moderate" :icon ""}
-  82 	{:description "Rain showers: violent" :icon ""}
-  85 	{:description "Snow showers: slight" :icon ""}
-  86 	{:description "Snow showers: heeavy" :icon ""}
-  95  {:description "* Thunderstorm" :icon ""}
-  96  {:description "* Thunderstorm with slight hail" :icon ""}
-  99  {:description "* Thunderstorm with heavy hail" :icon ""}
-})
-
+(def WMO 
+  {
+    0   {:description "Clear sky" :icon "󰖙"}
+    1   {:description "Mainly clear" :icon "󰖐"}
+    2   {:description "Partly cloudy" :icon ""}
+    3   {:description "Overcast" :icon "󰖐"}
+    45  {:description "Fog" :icon "󰖑"}
+    48  {:description "Fog" :icon "󰖑"}
+    51  {:description "Light drizzle" :icon "󰖗"}
+    53  {:description "Moderate drizzle" :icon "󰖗"}
+    55  {:description "Intense drizzle" :icon "󰖗"}
+    56  {:description "Light freezing drizzle" :icon "󰙿"}
+    57  {:description "Intense freezing drizzle" :icon "󰙿"}
+    61  {:description "Slight rain" :icon "󰖖"}
+    63  {:description "Moderate rain" :icon "󰖖"}
+    65  {:description "Heavy rain" :icon "󰖖"}
+    66  {:description "Light freezing rain" :icon "󰖒"}
+    67  {:description "Heavy freezing rain" :icon "󰖒"}
+    71  {:description "Light snow" :icon "󰖘"}
+    73  {:description "Moderate snow" :icon "󰖘"}
+    75  {:description "Heavy snow" :icon "󰼶"}
+    77  {:description "Snow grains" :icon "󰵼"}
+    80  {:description "Light rain showers" :icon ""}
+    81  {:description "Moderate rain showers" :icon ""}
+    82  {:description "Heavy rain showers" :icon ""}
+    85  {:description "Light snow showers" :icon ""}
+    86  {:description "Heavy snow showers" :icon ""}
+    95  {:description "Thunderstorms" :icon ""}
+    96  {:description "Thunderstorms with slight hail" :icon ""}
+    99  {:description "Thunderstorms with heavy hail" :icon ""}})
 
 (defn pull-temperature [data]
   (int (Math/floor (:temperature (:current_weather data)))))
@@ -56,14 +66,11 @@
    :units (pull-units data)
    :conditions (pull-conditions data)})
 
-(defn decode [s] (json/parse-string s true))
-(defn encode [s] (json/generate-string s))
-
 (->
-  API_BASE 
+  API_URL 
   http/get 
   :body
-  decode
+  (json/decode true)
   relevant
-  encode
+  json/encode
   println)
